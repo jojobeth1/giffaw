d607bb6682ea5c81b280925f450970f6d69926dd
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-07-07 08:48:46 -0700

    added script using jquery and ajaxand giphy API

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,34 @@
-$(document).on("ready", function(){
+console.log("sanity check");
 
+var endPoint = 'http://api.giphy.com/v1/gifs/search'
+
+$(document).ready( function(){
+ $('.form-inline').on('submit',function(event){
+   event.preventDefault();
+   $.ajax({
+     method:'GET',
+     url: endPoint,
+     dataType: 'json',
+     data: $("form").serialize(),
+     success: onSuccess
+   });
+ });
 });
+
+
+function onSuccess(json) {
+  /*if (json.pagination.offset === 0) {
+    $(".gif-img").remove();
+  }*/
+  json.data.forEach(function(v,i){
+    $(".gif-gallery").append($("<img class='img-responsive img-thumbnail gif-img' src="+v.images.fixed_height.url+">"));
+  });
+}
+
+
+/*function onSuccess(dataFromAPI){
+  //$('gif-gallery').html('<img src')
+  console.log(dataFromAPI)
+}
+var data = dataFromAPI;
+*/
